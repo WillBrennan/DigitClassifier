40f2ae97ff75522af3e9402c865e2af6b95a133e
Will Brennan <william.brennan@skytales.co.uk>
2015-06-15 00:04:31 +0100

    Added basic layers

---
+++ Layers.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Will Brennan'
+
+
+# Built-in Modules
+# Standard Modules
+# Custom Modules
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Will Brennan'
+
+
+# Built-in Modules
+import logging
+# Standard Modules
+import numpy
+import theano
+import theano.tensor as T
+from theano.tensor.signal import downsample
+from theano.tensor.nnet import conv
+# Custom Modules
+
+logger = logging.getLogger("main")
+
+
+class PoolingLayer(object):
+    def __init__(self, rng, input, filter_shape, image_shape, poolsize=(2, 2)):
+        logger.debug("Initialised Pooling Layer")
+        logger.debug("      image_shape: {0}".format(image_shape))
+        logger.debug("      filter_shape: {0}".format(filter_shape))
+        logger.debug("      poolsize: {0}".format(poolsize))
+        assert image_shape[1] == filter_shape[1]
+        self.input = input
+        fan_in = numpy.prod(filter_shape[1:])
+        fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) / numpy.prod(poolsize))
+        W_bound = numpy.sqrt(6. / (fan_in + fan_out))
+        self.W = theano.shared(numpy.asarray(rng.uniform(low=-W_bound, high=W_bound, size=filter_shape), dtype=theano.config.floatX), borrow=True)
+        b_values = numpy.zeros((filter_shape[0],), dtype=theano.config.floatX)
+        self.b = theano.shared(value=b_values, borrow=True)
+        conv_out = conv.conv2d(input=input, filters=self.W, filter_shape=filter_shape, image_shape=image_shape)
+        pooled_out = downsample.max_pool_2d(input=conv_out, ds=poolsize, ignore_border=True)
+        # Using tanh activation layer
+        self.output = T.tanh(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
+        self.params = [self.W, self.b]
+
+
+class LogisticRegression(object):
+    def __init__(self, input, n_in, n_out):
+        logger.debug("Initialised Logistic Regression with {0} input and {1} output units".format(n_in, n_out))
+        self.W = theano.shared(value=numpy.zeros((n_in, n_out), dtype=theano.config.floatX), name='W', borrow=True)
+        self.b = theano.shared(value=numpy.zeros((n_out,), dtype=theano.config.floatX), name='b', borrow=True)
+        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
+        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
+        self.params = [self.W, self.b]
+
+    def negative_log_likelihood(self, y):
+        return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
+
+    def cross_entropy(self, y):
+        # todo: Implement cross_entropy loss function
+        raise ValueError("This has not yet been implemented")
+
+    def errors(self, y):
+        # check if y has same dimension of y_pred
+        if y.ndim != self.y_pred.ndim:
+            raise TypeError('y should have the same shape as self.y_pred',('y', y.type, 'y_pred', self.y_pred.type))
+        # check if y is of the correct datatype
+        if y.dtype.startswith('int'):
+            # the T.neq operator returns a vector of 0s and 1s, where 1
+            # represents a mistake in prediction
+            return T.mean(T.neq(self.y_pred, y))
+        else:
+            raise NotImplementedError()
+
+
+class HiddenLayer(object):
+    def __init__(self, rng, input, n_in, n_out, W=None, b=None, activation=T.tanh, dropout=False):
+        logger.debug('Initialised HiddenLayer with {0} input and {1} output units'.format(n_in, n_out))
+        self.input = input
+        if W is None:
+            W_values = numpy.asarray(rng.uniform(
+                    low=-numpy.sqrt(6. / (n_in + n_out)),
+                    high=numpy.sqrt(6. / (n_in + n_out)),
+                    size=(n_in, n_out)
+                ),
+                dtype=theano.config.floatX
+            )
+            if activation == theano.tensor.nnet.sigmoid:
+                W_values *= 4
+            W = theano.shared(value=W_values, name='W', borrow=True)
+        if b is None:
+            b_values = numpy.zeros((n_out,), dtype=theano.config.floatX)
+            b = theano.shared(value=b_values, name='b', borrow=True)
+        self.W = W
+        self.b = b
+        lin_output = T.dot(self.input, self.W) + self.b
+        self.output = (lin_output if activation is None else activation(lin_output))
+        self.params = [self.W, self.b]
+

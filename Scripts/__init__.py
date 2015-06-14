ba307e8a360c59d6afe48b68cf0066abdb767ce7
Will Brennan <william.brennan@skytales.co.uk>
2015-06-15 00:01:30 +0100

    Added Scripts python package

---
+++ Scripts/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Will Brennan'
+
+
+# Built-in Modules
+# Standard Modules
+# Custom Modules
+
+from main import *
+from mlScripts import *
